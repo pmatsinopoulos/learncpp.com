1df84865a77a4717fb4eae8de80b789d2c255668
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-01-02 20:13:21 +0200

    0406 Fixed Width Integers And size_t

---
+++ 0406-fixed-width-integers-and-size-t/main.cpp
@@ -0,0 +1,14 @@
+#include <cstdint>
+#include <iostream>
+
+int main()
+{
+
+  std::int64_t x{10};
+
+  std::cout << "x = " << x << '\n';
+
+  std::cout << "sizeof(x) = " << sizeof(x) << '\n';
+
+  return 0;
+}
